5e577f28d8aa07badf5e948f6379d863ee12209b
Daniela Alexandra Ferreira <ferreira.danielaalexandra@gmail.com>
2025-04-02 21:48:59 -0300

    Alteraçoes

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,66 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+     
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            try
+            {
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null) 
+                    
+                    {
+
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"temperatura mínima: {t.temp_min} \n" +
+                                         $"temperatura máxima: {t.temp_max} \n";
+                                                                               
+
+
+
+
+
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão!";
+                    }
+
+
+                }
+                else
+
+                {
+
+                    lbl_res.Text = "Preencha a Cidade!";
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+
+            }
+
+
+
         }
     }
 
